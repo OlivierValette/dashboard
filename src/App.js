11b0daf3d316e0e41155cadbe34c6959bdf20e1d
Olivier Valette <olivier.valette@spi10.com>
2018-12-13 10:16:09 +0100

    Creating Counters widget call

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import './App.css';
 import 'animate.css';
 import Color from "./widgets/Color";
 import Dog from "./widgets/Dog";
+import Counters from "./widgets/Counters";
 
 class App extends Component {
-  render() {
+
+   render() {
     return (
       <Dashboard row={4} col={3} gutter={10} animationClassIn='animated fadeInDownBig' animationClassOut='animated bounceOut'>
         <Widget size='large'>
@@ -16,7 +18,69 @@ class App extends Component {
             <Dog/>
         </Widget>
         <Widget size='tall'>
-            <Counters/>
+            <Counters socialMedia={[
+                                    {
+                                        name: "twitter",
+                                        logo: "fa-twitter",
+                                        Counter: "Followers",
+                                        user: '',
+                                        key: {
+                                            name: '@DC_Rennes',
+                                            value: ''
+                                        },
+                                    },
+                                    {
+                                        name: "facebook",
+                                        logo: "fa-facebook",
+                                        Counter: "Likes",
+                                        user: '',
+                                        key: {
+                                            name: 'token',
+                                            value: '1627334644211864|3_tGe7MNSzumVcKxMyJUTqZAlz4',
+                                        },
+                                    },
+                                    {
+                                        name: "instagram",
+                                        logo: "fa-instagram",
+                                        Counter: "Followers",
+                                        user: '',
+                                        key: {
+                                            name: 'token',
+                                            value: '3216680391.5a20414.d66b30dc0857473dbf9723d34b502a73',
+                                        },
+                                    },
+                                    {
+                                        name: "linkedin",
+                                        logo: "fa-linkedin",
+                                        Counter: "Connections",
+                                        user: '',
+                                        key: {
+                                            name: 'oauth',
+                                            value: 'AQXo4yikouZnzWkUYZCOnVCKnhAALbi0-eSrlWTORI85p8dHCx_n0lnP7sG1k8HrOqPPvQ6Nkf0iAII2Ui34fsFr6IGcclgDnIsK_Z1Olx3vX-cJNfvuQoqNKFtUvhJOLE43iQQy0y1NDJXIDJV4nTPQCdezuaqAIEbjgcFa8tWsGmSZgtI'
+                                        },
+                                    },
+                                    {
+                                        name: "youtube",
+                                        logo: "fa-youtube",
+                                        Counter: "Subscribers",
+                                        user: '',
+                                        key: {
+                                            name: 'key',
+                                            value: 'AIzaSyDXpwzqSs41Kp9IZj49efV3CSrVxUDAwS0',
+                                        },
+                                    },
+                                    {
+                                        name: "github",
+                                        logo: "fa-github",
+                                        Counter: "Followers",
+                                        user: '',
+                                        key: {
+                                            name: '',
+                                            value: ''
+                                        },
+                                    },
+                                    ]}
+            />
         </Widget>
       </Dashboard>
     );
